6bed645844aae8e56b6c8ceb038ba6af284c1e35
Khandice <s.khandice@gmail.com>
2021-10-25 18:03:41 -0700

    Creates method in book Model that creates a dictionary from the Book's attributes

--- app/models/book.py
+++ app/models/book.py
@@ -5,3 +5,10 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
+
+    def make_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description
+        }
