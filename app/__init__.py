ff52a65a4e47ef140a750291484ff35a244fb1de
Khandice <s.khandice@gmail.com>
2021-11-03 09:18:28 -0700

    Updates to have paths for database housed within .env file successfully

--- app/__init__.py
+++ app/__init__.py
@@ -2,29 +2,37 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
-import os
 
+# This built-in module provides a way to read environment variables
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
+
+# The python-dotenv package specifies to call this method, which loads the values from our .env file so that the os module is able to see them.
 load_dotenv()
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    if test_config is None:
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    if not test_config:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
             "SQLALCHEMY_DATABASE_URI")
     else:
         app.config["TESTING"] = True
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
             "SQLALCHEMY_TEST_DATABASE_URI")
 
     db.init_app(app)
     migrate.init_app(app, db)
 
+    # registers the Book Model with app
+    from app.models.book import Book
+    
+    # Registers the routes and bluepreint to the app
     from .routes import books_bp
     app.register_blueprint(books_bp)
 
