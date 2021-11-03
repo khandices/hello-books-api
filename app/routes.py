fb8d17b0ff7f6849636466a2d7f1331bb1a3c724
Khandice <s.khandice@gmail.com>
2021-11-03 09:19:28 -0700

    Refactors routes into their own seperate methods

--- app/routes.py
+++ app/routes.py
@@ -4,45 +4,61 @@ from flask import Blueprint, jsonify, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-@books_bp.route("", methods=["POST", "GET"])
-def handle_books():
-    if request.method == 'POST':
-        request_body = request.get_json()
-        new_book = Book(title=request_body["title"],
-                        description=request_body["description"])
+@books_bp.route("", methods=["POST"])
+def create_book():
+    request_body = request.get_json()
+    new_book = Book(title=request_body["title"],
+                    description=request_body["description"])
 
-        db.session.add(new_book)
-        db.session.commit()
+    db.session.add(new_book)
+    db.session.commit()
 
-        return make_response(f"Book {new_book.title} successfully created", 201)
+    return make_response(f"Book {new_book.title} successfully created", 201)
     
-    elif request.method == 'GET':
-        books = Book.query.all()
-        books_response = []
-        for book in books:
-            books_response.append(book.to_dict())
-        return jsonify(books_response)
 
+@books_bp.route("", methods=["GET"])
+def read_books():
+    books = Book.query.all()
+    books_response = []
+    for book in books:
+        books_response.append(book.to_dict())
+    return jsonify(books_response)
+        
 
-@books_bp.route("/<book_id>", methods=["GET", "PUT", "DELETE"])
-def handle_book(book_id):
+@books_bp.route("/<book_id>", methods=["GET"])
+def read_book(book_id):
     book_id = int(book_id)
     book = Book.query.get(book_id)
+
     if book == None:
         return make_response("", 404)
-        
-    if request.method == "GET":
-        return book.to_dict()
+    
+    return book.to_dict()
 
-    elif request.method == "PUT":
+
+@books_bp.route("/<book_id>", methods=["PUT"])
+def update_book(book_id):
         form_data = request.get_json()
+        book_id = int(book_id)
+        book = Book.query.get(book_id)
+
+        if book == None:
+            return make_response("", 404)
 
         book.title = form_data["title"]
         book.description = form_data["description"]
         db.session.commit()
         return make_response(f"Book #{book.id} successfully updated")
     
-    elif request.method == "DELETE":
+
+@books_bp.route("/<book_id>", methods=["DELETE"])
+def delete_book(book_id):
+        book_id = int(book_id)
+        book = Book.query.get(book_id)
+
+        if book == None:
+            return make_response("", 404)
+
         db.session.delete(book)
         db.session.commit()
         return make_response(f"Book #{book.id} successfully deleted")
