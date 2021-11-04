cf0da1aa18280482f85980f7a540b9e70dbd8dd8
Khandice <s.khandice@gmail.com>
2021-11-03 18:55:54 -0700

    Creates Genre Model

---
+++ app/models/genre.py
@@ -0,0 +1,7 @@
+from app import db
+
+
+class Genre(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    # books = db.relationship("Book", back_populates="author")
